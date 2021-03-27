1dcc7adb8b5ed42092e9c1e94a89042319a821e3
saracrz <sara.crz10@gmail.com>
2021-03-27 10:21:05 +0000

    Added getSentiment function

--- scripts/newNote.js
+++ scripts/newNote.js
@@ -1,34 +1,52 @@
-const MonkeyLearn = require('monkeylearn');
-
 const saveButton = document.getElementById("Save");
 const textarea = document.getElementById("text-area");
 
-saveButton.addEventListener("click", (e) => {
-  const eventList = localStorage.getItem("events");
-  const date = new Date().toISOString();
+const getSentiment = (classifications) => {
+  let words = classifications.text.split(' ');
+  let largerClassification = 0;
 
-  if (eventList){
-    const list = JSON.parse(eventList);
-    const data = {
-      title: textarea.value,
-      start: date,
+  for(let i = 0; i < words.length; i++) {
+    if (words[i].length > largerClassification) {
+      largerClassification = words[i].length
     }
-    list.push(data);
-    localStorage.setItem("events", JSON.stringify(list));
-  } else {
-    const data = [{
-      title: textarea.value,
-      start: date,
-    }]
-    localStorage.setItem("events", JSON.stringify(data));
   }
+  return largerClassification;
+}
+
+saveButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  const payload = {
+    "data": [textarea.value]
+  }
+  fetch('https://api.monkeylearn.com/v3/classifiers/cl_pi3C7JiL/classify/', {
+    method: 'POST', body: JSON.stringify(payload),
+    headers: { Authorization: 'Token 82286876434f802e702da22ea9b886352bd4a071',
+    "Content-Type": "application/json" }
+  })
+  .then(res => res.json())
+  .then(res => {
+    console.log(res);
+    const eventList = localStorage.getItem("events");
+    const date = new Date().toISOString();
+  
+    if (eventList){
+      const list = JSON.parse(eventList);
+      const data = {
+        title: textarea.value,
+        start: date,
+        sentiment: getSentiment(),
+      }
+      list.push(data);
+      localStorage.setItem("events", JSON.stringify(list));
+    } else {
+      const data = [{
+        title: textarea.value,
+        start: date,
+      }]
+      localStorage.setItem("events", JSON.stringify(data));
+    }
+  })
 });
 
 
 
-const ml = new MonkeyLearn('[API_KEY]')
-let model_id = 'cl_pi3C7JiL'
-let data = ['This is a great tool!', {text: 'The location is excellent.', external_id: 'ANY_ID'}]
-ml.classifiers.classify(model_id, data).then(res => {
-    console.log(res.body)
-   })
